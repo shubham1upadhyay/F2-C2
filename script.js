05cbf2fd529e781e09ae98b81786f048cb8fe043
shubham1upadhyay <shubhamup47@gmail.com>
2023-05-20 17:42:00 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+//sample users list
+var users = [
+    { id: 1, name: "John", age: "18", profession: "developer" },
+    { id: 2, name: "Jack", age: "20", profession: "developer" },
+    { id: 3, name: "Karen", age: "19", profession: "admin" }
+  ];
+
+  function renderUser() {
+    var userList = document.getElementById("userList");
+    userList.innerHTML = "";
+
+    users.forEach(function(user) {
+      var list = document.createElement("li");
+
+      var name = document.createElement("p");
+      name.textContent = "Name: " + user.name;
+      list.appendChild(name);
+
+      var profession = document.createElement("p");
+      profession.textContent = "Profession: " + user.profession;
+      list.appendChild(profession);
+
+      var age = document.createElement("p");
+      age.textContent = "Age: " + user.age;
+      list.appendChild(age);
+      userList.appendChild(list);
+    });
+  }
+
+  //filter function
+  function filter() {
+    var selectedProfession = document.getElementById("profession").value;
+
+    if (selectedProfession === "") {
+      alert("Please select a profession before clicking the button.");
+      return;
+    }
+
+    var filterUser = users.filter(function(user) {
+      return user.profession === selectedProfession;
+    });
+
+    users = filterUser;
+    renderUser();
+  }
+
+  //add function
+  
+  function addUser() {
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var profession = document.getElementById("newProfession").value;
+
+    var newUser = {
+      id: users.length + 1,
+      name: name,
+      age: age,
+      profession: profession
+    };
+
+    users.push(newUser);
+    renderUser();
+
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("newProfession").value = "developer";
+  }
+  renderUser();
